3b8a2edf13da59b1e3fc77cf42a8b16eb2f88ffe
zhvirblis <yurocker@mail.ru>
2016-11-27 17:56:58 +0300

    automatization(Watch)

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,18 +7,28 @@ module.exports = function(grunt) {
 				src: [
 					'public/javascripts/*.js'
 				],
-				dest: 'public/javascripts/build.js'
+				dest: 'public/javascripts/build/script.js'
 			}
 		},
 		uglify: {
 			build: {
-				src: 'public/javascripts/build.js',
-				dest: 'public/javascripts/build.min.js'
+				src: 'public/javascripts/build/script.js',
+				dest: 'public/javascripts/build/script.min.js'
 			}
+		},
+		watch: {
+    		scripts: {
+        	files: ['public/javascripts/*.js'],
+        	tasks: ['concat', 'uglify'],
+       		options: {
+            		spawn: false
+       			 }
+    		}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.registerTask('default', ['concat', 'uglify']);
 };
